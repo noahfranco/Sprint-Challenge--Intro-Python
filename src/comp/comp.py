a98bd1ab8ff01bde0efde19b0fdb94e4d552ae0c
noahfranco <noahfra@gmail.com>
2020-01-17 17:11:34 -0500

    fiished project, got data on all files to print, and passed two test

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -48,21 +48,21 @@ print(d)
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = [f"{i.name} - {i.age}" for i in humans]
+e = [f"{i.name}-{i.age}" for i in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = [(i.name, i.age) for i in humans if range(27, 32)]
+f = [(i.name, i.age) for i in humans if int(i.age) in range(27, 33)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = [(i.name.upper(), i.age + 5) for i in humans]
+g = [[i.name.upper(), i.age + 5] for i in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,11 +3,18 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
     # TODO
+
+    def __str__(self):
+        return "GroundVehicle"
+
+    def drive(self):
+        return "vroooom"
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -16,6 +23,16 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(num_wheels=2)
+
+    def __str__(self):
+        return "Motorcycle"
+
+    def drive(self):
+        return "BRAAAP!!"
 
 # TODO
 
@@ -30,3 +47,6 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 # TODO
+
+for dub in vehicles:
+    print(f"{dub} has {dub.num_wheels} wheels and sounds like {dub.drive()}")
